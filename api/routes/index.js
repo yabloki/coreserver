522e1bab92f186f066a6ed6d1ab5f344b688e9b5
Kirill Moizik <moizik@gmail.com>
2020-02-05 00:01:41 -0500

    add ui sso

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var auth = require('./auth')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/auth', auth);
+
+router.get('/auth2', function(req, res, next) {
+  res.redirect('localhost:8080/api/oauth/sso/callback?payload=PAYLOAD&hmac=HMAC');
 });
 
 module.exports = router;
