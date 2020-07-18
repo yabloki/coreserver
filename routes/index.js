f434dd258367a62834e9a3d45778ea6cc4f340d1
Kirill Moizik <moizik@gmail.com>
2020-07-18 15:30:50 -0400

    fix links

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ const {
 } = require('@lib/auth');
 
 const generateNonce = require('@lib/nonce-generator.js');
-var cors = require('cors')
 
 var router = express.Router();
 
@@ -67,7 +66,7 @@ router.post('/post', async function (req, res, next) {
   res.json(data)
 });
 
-router.post('/signup', cors(), async function (req, res, next) {
+router.post('/signup', async function (req, res, next) {
   if (!verifyToken(req.body.token, req.body.hmac) ||
       !await verifySignature(req.body.signature, req.body.address)) {
         res.send('verification error');
@@ -80,7 +79,7 @@ router.post('/signup', cors(), async function (req, res, next) {
       payloadSign
     } = await processSignUpData(req.body.token, req.body.hmac, req.body.signature, req.body.address)
     payload = Buffer.from(JSON.stringify(payload), 'utf8').toString('hex');
-    let url = 'http://localhost:8080/api/oauth/sso/callback?payload=' + payload + "&hmac=" + payloadSign
+    let url = 'http://commento.2cents.media/api/oauth/sso/callback?payload=' + payload + "&hmac=" + payloadSign
     res.redirect(url)
   } catch (error) {
     console.log(error)
