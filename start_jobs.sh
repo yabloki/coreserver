d157ffdcf89f571bae3fbd8393c4e7412e7f6f0d
Kirill Moizik <moizik@gmail.com>
2020-07-24 13:06:14 -0400

    add jobs

--- start_jobs.sh
+++ start_jobs.sh
@@ -1,3 +1,3 @@
 sudo docker build -t coreserver:latest  .;
 sudo docker-compose down;
-sudo docker-compose -f docker-compose-jobs.yml down; sudo -f docker-compose-jobs.yml docker-compose up -d;
+sudo docker-compose -f docker-compose-jobs.yml down; sudo docker-compose -f docker-compose-jobs.yml up -d;
